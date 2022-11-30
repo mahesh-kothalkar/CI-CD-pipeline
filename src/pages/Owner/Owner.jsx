7d4754787737bd6e3aa7aa7eab9a971bc64b869a
Mahesh <mkothalkar04@gmail.com>
2022-11-30 14:07:29 +0530

    "Added images"

--- src/pages/Owner/Owner.jsx
+++ src/pages/Owner/Owner.jsx
@@ -18,17 +18,24 @@ const Owner = () => {
                 <h2 className='owenername'>Owner Page</h2>
                 
                     <div className="items">
-                        <div className='propname'>Owner Name: </div>
-                        <div className='propemail'>Owner Email: </div>
-                        <div className='propaddress'>Owner Address: </div>
+                        <div className='propname'>Owner Name:Leanne Graham </div>
+                        <div className='propemail'>Owner Email: Sincere@april.biz</div>
+                        <div className='propaddress'>Owner Address: Kulas Light, Apt. 556, Gwenborough, 92998-3874</div>
                     </div>
                     <button onClick={() => handleAdd()} >Add Property</button>
               
                 <div>
                     <div className="item" >
-                        <div className='card'> Name: </div>
-                        <div className='address'> Address: </div>
-                        <img className='img' src="" alt='img' /><br />
+                        <div className='card'> Name: Treebo Trend Parkmahesh</div>
+                        <div className='address'> Address: Survey No 161 Behind Laxman Villa Bhosale Nagar Hadapsar, Pune, 411028, 411028, Pune, India</div>
+                        <img className='img' src="https://imgcy.trivago.com/c_limit,d_dummy.jpeg,f_auto,h_470,q_auto,w_805//partnerimages/53/24/532468774.jpeg" alt='img' /><br />
+                        <button onClick={() => handleEdit()} >Edit </button>
+                        <button >Delete </button>
+                    </div>
+                    <div className="item" >
+                        <div className='card'> Name: Meluha The Fernmhahbrejhwb</div>
+                        <div className='address'> Address: Central Avenue, Hiranandani Gardens, Powai, 400076, Mumbai, India</div>
+                        <img className='img' src="https://imgcy.trivago.com/c_limit,d_dummy.jpeg,f_auto,h_470,q_auto,w_805//partnerimages/63/08/630813794.jpeg" alt='img' /><br />
                         <button onClick={() => handleEdit()} >Edit </button>
                         <button >Delete </button>
                     </div>
--- src/pages/Tenant/Tenant.jsx
+++ src/pages/Tenant/Tenant.jsx
@@ -9,10 +9,16 @@ const Tenant = () => {
                 <h2 className='name'>Welcome Tenant</h2>
                 <>
                     <div className="item">
-                        <div className='card'>Name</div>
+                        <div className='card'>Name:Treebo Trend Parkmahesh </div>
                         <br />
-                        <div className='address'>property Address</div>
-                        <img className='tenantpageimg' src="" alt='img'/>
+                        <div className='address'>property : Survey No 161 Behind Laxman Villa Bhosale Nagar Hadapsar, Pune, 411028, 411028, Pune, India</div>
+                        <img className='tenantpageimg' src="https://imgcy.trivago.com/c_limit,d_dummy.jpeg,f_auto,h_470,q_auto,w_805//partnerimages/53/24/532468774.jpeg" alt='img'/>
+                    </div>
+                    <div className="item">
+                        <div className='card'>Name:Meluha The Fernmhahbrejhwb</div>
+                        <br />
+                        <div className='address'>property :Central Avenue, Hiranandani Gardens, Powai, 400076, Mumbai, India</div>
+                        <img className='tenantpageimg' src="https://imgcy.trivago.com/c_limit,d_dummy.jpeg,f_auto,h_470,q_auto,w_805//partnerimages/63/08/630813794.jpeg" alt='img'/>
                     </div>
                 </>
             </div>
